b86663453477f737bb15918c60dbfaa6abf57e2a
username-bx <57657122+username-bx@users.noreply.github.com>
2025-11-27 15:40:07 +0000

    Deploying to gh-pages from @ username-bx/tangyuans@6f5cdfd50ca242a8a8465599eb64d661e8afdcfc 🚀

---
+++ assets/index.html-BsdMYWY-.js
@@ -0,0 +1,18 @@
+import{_ as n}from"./plugin-vue_export-helper-DlAUqK2U.js";import{c as t,a as e,o}from"./app-BCd1aVVa.js";const m={};function a(r,p){return o(),t("div",null,[e(` ### 安装依赖
+
+\`\`\`
+// corepack enable
+// pnpm -v
+
+pnpm i
+
+\`\`\`
+
+
+### 开发模式
+
+\`\`\`
+
+npm run dev
+
+\`\`\` `),e(" 这是一个博客主页的案例。 "),e(" 要使用此布局，你应该在页面前端设置 `layout: BlogHome` 和 `home: true`。 "),e(" 相关配置文档请见 [博客主页](https://theme-hope.vuejs.press/zh/guide/blog/home/)。 ")])}const s=n(m,[["render",a]]),l=JSON.parse('{"path":"/","title":"主页","lang":"zh-CN","frontmatter":{"home":true,"layout":"Blog","icon":"house","title":"主页","hero":true,"heroImage":"/sun.svg","image":"/sun.svg","heroText":"汤圆随笔","heroFullScreen":true,"tagline":"前端开发记录","headline":"blog home","url":"/","projects":null,"footer":"汤圆随笔记录","head":[["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"WebPage\\",\\"name\\":\\"主页\\"}"],["meta",{"property":"og:url","content":"https://mister-hope.github.io/"}],["meta",{"property":"og:site_name","content":"汤圆"}],["meta",{"property":"og:title","content":"主页"}],["meta",{"property":"og:type","content":"website"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2025-11-12T07:38:33.000Z"}],["meta",{"property":"article:modified_time","content":"2025-11-12T07:38:33.000Z"}]]},"git":{"createdTime":1737523887000,"updatedTime":1762933113000,"contributors":[{"name":"benjamin","username":"benjamin","email":"benjamin@manka.co.jp","commits":2,"url":"https://github.com/benjamin"},{"name":"Benjamin","username":"Benjamin","email":"benjaminma@example.com","commits":4,"url":"https://github.com/Benjamin"}]},"readingTime":{"minutes":0.85,"words":254},"filePathRelative":"README.md","excerpt":"<!-- ### 安装依赖\\n\\n```\\n// corepack enable\\n// pnpm -v\\n\\npnpm i\\n\\n```\\n\\n\\n### 开发模式\\n\\n```\\n\\nnpm run dev\\n\\n``` -->\\n<!-- 这是一个博客主页的案例。 -->\\n<!-- 要使用此布局，你应该在页面前端设置 `layout: BlogHome` 和 `home: true`。 -->\\n<!-- 相关配置文档请见 [博客主页](https://theme-hope.vuejs.press/zh/guide/blog/home/)。 -->\\n"}');export{s as comp,l as data};
